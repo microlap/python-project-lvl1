7c7c33ab970a4a8bbd6785c2d8195f145bb4cbf2
mcrlb <iraanov@gmail.com>
2022-08-05 03:36:24 +0300

    Modified script after flake8 review

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,29 +3,31 @@ import random
 import prompt
 
 
-print('Welcome to the Brain Games!')
+def main():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print('Hello,', name)
+    print('Answer "yes" if the number is even, otherwise answer "no".')
 
-name = prompt.string('May I have your name? ')
-print('Hello,', name)
+    def is_even():
+        count = 0
+        ans_pos = 'yes'
+        ans_neg = 'no'
+        while count != 3:
+            n = round(random.random() * 100)
+            print('Question:', n)
+            answer = input('Your answer: ')
+            if (n % 2 == 0 and answer == ans_pos) or \
+               (n % 2 != 0 and answer == ans_neg):
+                print('Correct')
+                count += 1
+            else:
+                print(f"'yes' is wrong answer ;(. Correct answer was 'no'. \n Let's try again, {name}")
+                count = 0
+        print(f'Congratulations, {name}!')
 
-print('Answer "yes" if the number is even, otherwise answer "no".')
-
-
-def is_even():
-    count = 0
-    ans_pos = 'yes'
-    ans_neg = 'no'
-    while count != 3:
-        n = round(random.random() * 100)
-        print('Question:', n)
-        answer = input('Your answer: ')
-        if (n % 2 == 0 and answer == ans_pos) or (n % 2 != 0 and answer == ans_neg):
-            print('Correct')
-            count += 1
-        else:
-            print("'yes' is wrong answer ;(. Correct answer was 'no'. \n Let's try again, Bill!")
-            count = 0
-    print('Congratulations,', name, '!')
+    is_even()
 
 
-is_even()
+if __name__ == '__main__':
+    main()
